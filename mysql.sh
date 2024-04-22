1bfd3284a5496d24f22172de2e73e5cac74d1cf8
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-22 09:30:16 +0530

    hellow script

--- mysql.sh
+++ mysql.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Starting MySQL Server"
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h mysql.daws78s.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
